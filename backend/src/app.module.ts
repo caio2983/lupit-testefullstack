c13e16a5ddc06e2bcdf59140fdc092bb96ff2441
caio2983 <caioluna2002@gmail.com>
2025-07-24 23:49:27 -0300

    feat(backend): Update AppModule to include Team module and Prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TeamService } from './team/team.service';
+import { TeamController } from './team/team.controller';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TeamController],
+  providers: [AppService, TeamService, PrismaService],
 })
 export class AppModule {}
