15e1bfcf2e4a84a90e17545c57ca4031f287ae35
caio2983 <caioluna2002@gmail.com>
2025-08-04 03:21:17 -0300

    feat(frontend): update team card

--- frontend/src/app/components/ProfileCards/TeamCard/TeamCard.tsx
+++ frontend/src/app/components/ProfileCards/TeamCard/TeamCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Team } from "../../../../../types/team";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function TeamCard({
   team,
@@ -16,20 +17,22 @@ export default function TeamCard({
   }
 
   return (
-    <div className="profile-card">
-      <div className="profile-card-image-wrapper">
-        <Image
-          src={team?.image ? team.image : "/Knight.png"}
-          alt="Imagem do time"
-          fill
-        />
+    <Link href={`/times/${team?.id}`}>
+      <div className="profile-card team-card">
+        <div className="profile-card-image-wrapper">
+          <Image
+            src={team?.image ? team.image : "/Knight.png"}
+            alt="Imagem do time"
+            fill
+          />
+        </div>
+        <div className="profile-card-info">
+          <p className="profile-card-label">{team?.name}</p>
+          <p className="profile-card-detail">
+            {numberOfPlayers} {numberOfPlayers === 1 ? "jogador" : "jogadores"}
+          </p>
+        </div>
       </div>
-      <div className="profile-card-info">
-        <p className="profile-card-label">{team?.name}</p>
-        <p className="profile-card-detail">
-          {numberOfPlayers} {numberOfPlayers === 1 ? "jogador" : "jogadores"}
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 }
