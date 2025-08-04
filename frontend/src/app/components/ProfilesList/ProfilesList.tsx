6b4d285d8a75b25bbfc26fc7b522d6c1b5611c95
caio2983 <caioluna2002@gmail.com>
2025-08-04 04:02:13 -0300

    feat(frontend): clean profile list's code and add routing to team page

--- frontend/src/app/components/ProfilesList/ProfilesList.tsx
+++ frontend/src/app/components/ProfilesList/ProfilesList.tsx
@@ -2,11 +2,12 @@
 
 import { Edit, Trash } from "lucide-react";
 import { Player } from "../../../../types/player";
-import { Team, TeamWithPlayerCount } from "../../../../types/team";
+import { TeamWithPlayerCount } from "../../../../types/team";
 import { useRouter } from "next/navigation";
 import Swal from "sweetalert2";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function ProfilesList({
   type,
@@ -25,11 +26,9 @@ export default function ProfilesList({
 
   useEffect(() => {
     if (typeof fetchTeams === "function") {
-      console.log("TESTEEEE");
       const loadTeams = async () => {
         try {
           const teamsData = await fetchTeams();
-
           setTeams(teamsData);
         } catch (error) {
           console.error(error);
@@ -62,93 +61,96 @@ export default function ProfilesList({
         <div className="profiles-property">Ações</div>
       </div>
 
-      {data.map((profile) => (
-        <div
-          key={profile.id}
-          className="profiles-card"
-          onClick={() => {
-            if (type === "team") {
-              router.push(`/times/${profile.id}`);
-            }
-          }}
-        >
-          <div className="profiles-property profiles-image">
-            <Image
-              src={profile.image ? profile.image : "/Knight.png"}
-              alt="Logo"
-              fill
-            />
-          </div>
-          <div className="profiles-property">{profile.id}</div>
-          <div className="profiles-property">{profile.name}</div>
-          <div className="profiles-property">
-            {type === "team" ? (
-              (profile as TeamWithPlayerCount).numberOfPlayers
-            ) : (
-              <>
-                <div className="profiles-little-image">
-                  <Image
-                    src={
-                      teams?.find(
-                        (team) => team.id === (profile as Player).team_id
-                      )?.image ?? "/Knight.png"
-                    }
-                    alt="Logo"
-                    fill
-                  />
-                </div>
-                <div className="team-name">
-                  {teams?.find(
-                    (team) => team.id === (profile as Player).team_id
-                  )?.name ?? ""}
-                </div>
-              </>
-            )}
-          </div>
-          <div className="profiles-property profiles-actions">
-            <Edit
-              className="icon"
-              size={25}
-              color="gray"
-              onClick={(e) => {
-                e.stopPropagation();
-                if (type === "team") {
-                  router.push(`/times/${profile.id}/editar`);
-                } else {
-                  router.push(`/jogadores/${profile.id}/editar`);
-                }
-              }}
-            />
-            <Trash
-              className="icon"
-              size={25}
-              color="gray"
-              onClick={async (e) => {
-                e.stopPropagation();
-                const resultado = await Swal.fire({
-                  title: "Tem certeza?",
-                  text:
-                    type === "player"
-                      ? "Remover o jogador é uma ação irreversível"
-                      : "Remover o time é uma ação irreversível",
-                  icon: "warning",
-                  showCancelButton: true,
-                  confirmButtonColor: "#d33",
-                  cancelButtonColor: "#3085d6",
-                  confirmButtonText: "Sim",
-                  cancelButtonText: "Não",
-                  scrollbarPadding: false,
-                  heightAuto: false,
-                });
+      {data.map((profile) => {
+        const isPlayer = type === "player";
+        const team = isPlayer
+          ? teams?.find((team) => team.id === (profile as Player).team_id)
+          : null;
 
-                if (resultado.isConfirmed && deleteProfile) {
-                  deleteProfile(profile.id);
-                }
-              }}
-            />
+        return (
+          <div
+            key={profile.id}
+            className={`profiles-card${
+              type === "team" ? " profiles-team-card" : ""
+            }`}
+            onClick={() => {
+              if (type === "team") {
+                router.push(`/times/${profile.id}`);
+              }
+            }}
+          >
+            <div className="profiles-property profiles-image">
+              <Image
+                src={profile.image ? profile.image : "/Knight.png"}
+                alt="Logo"
+                fill
+              />
+            </div>
+            <div className="profiles-property">{profile.id}</div>
+            <div className="profiles-property">{profile.name}</div>
+            <div className="profiles-property">
+              {type === "team" ? (
+                (profile as TeamWithPlayerCount).numberOfPlayers
+              ) : (
+                <>
+                  <div className="profiles-little-image">
+                    <Image src={team?.image ?? "/Knight.png"} alt="Logo" fill />
+                  </div>
+                  <div className="team-name">
+                    {team ? (
+                      <Link href={`/times/${team.id}`}>{team.name}</Link>
+                    ) : (
+                      "Sem time"
+                    )}
+                  </div>
+                </>
+              )}
+            </div>
+            <div className="profiles-property profiles-actions">
+              <Edit
+                className="icon"
+                size={25}
+                color="gray"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  if (type === "team") {
+                    router.push(`/times/${profile.id}/editar`);
+                  } else {
+                    router.push(`/jogadores/${profile.id}/editar`);
+                  }
+                }}
+              />
+              <Trash
+                className="icon"
+                size={25}
+                color="gray"
+                onClick={async (e) => {
+                  e.stopPropagation();
+                  const resultado = await Swal.fire({
+                    title: "Tem certeza?",
+                    text:
+                      type === "player"
+                        ? "Remover o jogador é uma ação irreversível"
+                        : "Remover o time é uma ação irreversível",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#d33",
+                    cancelButtonColor: "#3085d6",
+                    confirmButtonText: "Sim",
+                    cancelButtonText: "Não",
+                    scrollbarPadding: false,
+                    heightAuto: false,
+                  });
+
+                  if (resultado.isConfirmed && deleteProfile) {
+                    deleteProfile(profile.id);
+                  }
+                }}
+              />
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
